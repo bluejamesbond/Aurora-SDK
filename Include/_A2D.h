5f093840fad4d5d2b001b6162c55a9ebcb91d98b
Mathew Kurian <bluejamesbond@gmail.com>
2014-01-07 18:07:26 -0600

    Semantics fix

--- Include/_A2D.h
+++ Include/_A2D.h
@@ -1,27 +1,18 @@
 
 // For internal use only
 
+#ifndef GRAPHICS__
 #ifdef _WIN32
-
-#ifndef GRAPHICS__
 #define	 GRAPHICS__	"../Windows/Graphics.h"
-#endif
-
-#ifndef GRAPHICS
-#define	 GRAPHICS	"../../../Aurora-SDK/Include/Windows/Graphics.h"
-#endif
-
-#endif
-
-// Required for Linux
-#ifdef _LINUX
-
-#ifndef GRAPHICS__
+#elif _LINUX
 #define	 GRAPHICS__	"../Linux/Graphics.h"
 #endif
-
-#ifndef GRAPHICS
-#define	 GRAPHICS "../../../Aurora-SDK/Include/Linux/Graphics.h"
 #endif
 
+#ifndef GRAPHICS
+#ifdef _WIN32
+#define	 GRAPHICS	"../../../Aurora-SDK/Include/Windows/Graphics.h"
+#elif _LINUX
+#define	 GRAPHICS "../../../Aurora-SDK/Include/Linux/Graphics.h"
+#endif
 #endif
