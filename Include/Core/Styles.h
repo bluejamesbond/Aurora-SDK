4317016c84b3e5781c98e599222cda8af3555fa9
Mathew Kurian <bluejamesbond@gmail.com>
2014-01-16 10:47:06 -0600

    Broken version. Adding new code. Transferring PCs.

--- Include/Core/Styles.h
+++ Include/Core/Styles.h
@@ -55,21 +55,84 @@ namespace A2D{
 
 		enum Background
 		{
-			REPEAT_X_Y,
-			STRETCH_WIDTH_HEIGHT,
-			COVER_TOP_LEFT,           // Implemented
-			COVER_TOP_CENTER,
-			COVER_TOP_RIGHT,
-			COVER_CENTER_LEFT,
-			COVER_CENTER_CENTER,      // Implemented
-			COVER_CENTER_RIGHT,
-			COVER_BOTTOM_LEFT,
-			COVER_BOTTOM_CENTER,
-			COVER_BOTTOM_RIGHT
+			REPEAT,
+			STRETCH,
+			COVER
+		};
+
+		enum BackgroundPosition
+		{
+			CENTER,
+			LEFT,
+			TOP,
+			BOTTOM,
+			RIGHT,
+			CUSTOM
+		};
+
+		enum BackgroundRepeat
+		{
+			X,
+			Y,
+			X_Y,
+			NONE
 		};
 
 		static float AUTO;
+		
+		struct BackgroundProperties
+		{
+			Background m_style;
 
+			/**************************************/
+
+			Units m_customPositionXUnits;
+			Units m_customPositionYUnits;
+
+			int m_customPositionX;
+			int m_customPositionY;
+
+			/**************************************/
+
+			Units m_customSizeWidthUnits;
+			Units m_customSizeHeightUnits;
+
+			int m_customSizeWidth;
+			int m_customSizeHeight;
+
+			/**************************************/
+
+			BackgroundPosition m_positionX;
+			BackgroundPosition m_positionY;
+
+			BackgroundProperties() :
+				m_style(Background::COVER),
+				m_positionX(BackgroundPosition::CENTER),
+				m_positionY(BackgroundPosition::CENTER),
+				m_customPositionX(0),
+				m_customPositionY(0)
+			{
+			}
+
+			void setStyle(Background x_style)
+			{
+				m_style = x_style;
+			}
+
+			void setPosition(BackgroundPosition x_positionX, BackgroundPosition x_positionY)
+			{
+				m_positionX = x_positionX;
+				m_positionY = x_positionY;
+			}
+
+			void setCustomPosition(int x_customPositionX, int x__customPositionY)
+			{
+				// Note that customPosition will only be taken into consideration
+				// iff BackgroundPosition is set to PRECISE
+				m_customPositionX = x_customPositionX;
+				m_customPositionY = x__customPositionY;
+			}
+		};
 	};
 }
 
