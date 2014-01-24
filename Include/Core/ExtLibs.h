5dff2fdce3d1d8f8a41c85c5c14b0dac8e954605
bluejamesbond <bluejamesbond@gmail.com>
2014-01-24 06:28:03 -0500

    Basic changes. Testing.

--- Include/Core/ExtLibs.h
+++ Include/Core/ExtLibs.h
@@ -21,27 +21,7 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
-#ifdef _WIN32
-#include <tchar.h>
-#endif
 #include <wchar.h>
 #include <math.h>
 #include <iostream> 
 #include <fstream>
-
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<< MOVE THIS
-#ifdef _WIN32
-
-#pragma comment(lib, "d3d10.lib")
-#pragma comment(lib, "d3dx10.lib")
-#pragma comment(lib, "dxgi.lib")
-
-#include <d3d10.h>
-
-#include "C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Include\D3DX9.h"
-#include "C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Include\D3DX10.h"
-#include "C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Include\D3DX10math.h"
-#include "C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Include\D3DX9tex.h"
-
-#endif
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< TO EXTLIBS.H FOR WINDOWS
