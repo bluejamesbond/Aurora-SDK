a38649a9d8640b0a62425931c03a2e641b26ff7c
bluejamesbond <bluejamesbond@gmail.com>
2014-01-24 23:32:06 -0500

    Switched to GenericShader

---
+++ Include/Linux/GenericShader.h
@@ -0,0 +1,69 @@
+////////////////////////////////////////////////////////////////////////////////
+// GAURDS
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __ABSTRACTSHADER_H__
+#define __ABSTRACTSHADER_H__
+
+//+-----------------------------------------------------------------------------
+//
+//  Class: 
+//      TEXTURESHADER
+//
+//  Synopsis:
+//      Texture quad.
+//
+//------------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+// INCLUDE
+////////////////////////////////////////////////////////////////////////////////
+
+#include "ExtLibs.h"
+#include "GLShaderUtils.h"
+#include "Texture.h"
+#include "Common.h"
+
+namespace A2D {
+
+	////////////////////////////////////////////////////////////////////////////////
+	// DECLARATION
+	////////////////////////////////////////////////////////////////////////////////
+
+	class GenericShader
+	{
+	public:
+
+		GenericShader();
+		virtual ~GenericShader();
+
+	protected:
+
+
+	private:
+        //variables
+        char                    *       vshadername;
+        char                    *       fshadername;
+
+	public:
+        //variables
+        GLuint                          programID;
+        GLuint                          TextureID;
+        Texture                 *       aTexture;
+
+        //methods
+		void							renderShader();
+        void                            setVShader(char * xshader);
+        void                            setFShader(char * xshader);
+        void                            associateTexture(Texture * xTexture);
+
+		//////////////////////////////////////////////////////////
+		// ABSTRACT IMPLEMENTATION
+		//////////////////////////////////////////////////////////
+
+		virtual HRESULT	                initialize();
+
+	};
+}
+
+#endif
---
+++ Lib/Source/Linux/GenericShader.cpp
@@ -0,0 +1,64 @@
+#include "../../../Include/Linux/ExtLibs.h"
+#include "../../../Include/Linux/GenericShader.h"
+
+using namespace A2D;
+
+GenericShader::GenericShader() {}
+
+GenericShader::~GenericShader()
+{
+    glDeleteProgram(programID);
+    glDeleteTextures(1, &TextureID);
+
+}
+
+void GenericShader::setVShader(char * xshader)
+{
+    vshadername = xshader;
+}
+
+void GenericShader::setFShader(char * xshader)
+{
+    fshadername = xshader;
+}
+
+void GenericShader::associateTexture(Texture * xTexture)
+{
+    aTexture = xTexture;
+
+    //load all necessary textures with corresponding ID's, need to allow multiple(?)
+    TextureID  = glGetUniformLocation(programID, "basic_texture");
+}
+
+
+HRESULT GenericShader::initialize()
+{
+    //set shader path?
+    //temp FOR TESTING
+    if(vshadername == NULL)
+    {
+        vshadername = "/home/syk435/Testing Gl/GL-Test/SimpleVertexShader.vertexshader";
+    }
+
+    if(fshadername == NULL)
+    {
+        fshadername = "/home/syk435/Testing Gl/GL-Test/SimpleFragmentShader.fragmentshader";
+    }
+
+    // create shaders , tie to "programid". Maybe later make more shaders and tie to different IDs
+    programID = GLShaderUtils::LoadEffectFromFile(vshadername, fshadername);
+}
+
+void GenericShader::renderShader()
+{
+    // Use our shader
+    glUseProgram(programID);
+
+    //might need texture::render to come here
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, aTexture->tex);
+
+    // Set our "myTextureSampler" sampler to user Texture Unit 0
+    glUniform1i(TextureID, 0);
+
+}
